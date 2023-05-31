88276a131b0c34aed7a39b76c747b7a8fc76c915
@Macedo07 <lcmacedo07@gmail.com>
2023-05-31 19:09:53 -0300

    ADD COMPONENTE DE NOVO REGISTRO

---
+++ components/Button/Add.vue
@@ -0,0 +1,9 @@
+<template>
+  <button
+    class="mb-2 flex rounded text-xs font-medium px-3 bg-lime-300 hover:bg-lime-500 hover:text-white focus:bg-lime-500 focus:text-white h-9 items-center"
+    @click="$emit('add')"
+  >
+    NOVO REGISTRO
+    <div class="i-carbon:add-alt text-lg ml-2" />
+  </button>
+</template>
