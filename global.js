3cb7d543e4d0b796d85b92159215812f200db714
@Macedo07 <lcmacedo07@gmail.com>
2023-05-31 19:08:21 -0300

    ADD CONFS DO GLOBAL

---
+++ global.js
@@ -0,0 +1,21 @@
+export const _token = '_token'
+export const _user = '_user'
+export const _organ = '_organ'
+export const _permissions = '_permissions'
+export const adminSuper = 'super'
+
+export const appname = '----'
+export const titlePage = '------'
+export const description = '------'
+export const keywords = '------'
+
+export default {
+  _token,
+  _user,
+  _organ,
+  _permissions,
+  titlePage,
+  appname,
+  adminSuper,
+  keywords,
+}
