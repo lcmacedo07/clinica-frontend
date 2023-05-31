efab92fb38c03a56254c922e3ad8b0920569b357
@Macedo07 <lcmacedo07@gmail.com>
2023-05-31 19:10:10 -0300

    ADD COMPONENTE DE DELETAR E EDITAR

---
+++ components/Button/Delete.vue
@@ -0,0 +1,26 @@
+<script setup>
+defineProps({
+  title: String,
+})
+defineEmits(['delete'])
+</script>
+
+<template>
+  <!-- <button
+    @click="$emit('delete')"
+    class="flex rounded text-white text-xs text-center font-medium px-2.5 py-1.5 leading-normal ml-2 bg-red-500 hover:bg-red-700"
+  >
+    <div class="i-carbon:trash-can mr-1 mt-0.5" />
+    <span v-if="title">{{ title }}</span>
+    <span v-else> EXCLUIR </span>
+  </button> -->
+
+  <div
+    style="display: inline-flex; align-items: center; cursor: pointer;"
+    @click="$emit('delete')"
+  >
+    <div class="i-carbon:trash-can ml-2" @click="$emit('delete')" />
+    <span v-if="title">{{ title }}</span>
+  </div>
+</template>
+
---
+++ components/Button/Edit.vue
@@ -0,0 +1,26 @@
+<script setup>
+defineProps({
+  title: String,
+})
+defineEmits(['edit'])
+</script>
+
+<template>
+  <!-- <button
+    @click="$emit('edit')"
+    class="flex rounded text-white text-xs text-center font-medium px-2.5 py-1.5 leading-normal ml-2 bg-lime-500 hover:bg-lime-700"
+  >
+    <div class="i-carbon:pen mr-1 mt-0.5" />
+    <span v-if="title">{{ title }}</span>
+    <span v-else> EDITAR </span>
+  </button> -->
+
+  <div
+    style="display: inline-flex; align-items: center; cursor: pointer;"
+    @click="$emit('edit')"
+  >
+    <div class="i-carbon:edit ml-2" @click="$emit('edit')" />
+    <span v-if="title">{{ title }}</span>
+  </div>
+</template>
+
