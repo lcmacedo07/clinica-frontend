e8a0e849b770700defc84d3cb3dff7ada90614f4
@Macedo07 <lcmacedo07@gmail.com>
2023-05-31 19:08:01 -0300

    ADD CONF PADRAO DO VUE

---
+++ app.vue
@@ -0,0 +1,18 @@
+<template>
+  <NuxtLayout>
+    <NuxtitlePage />
+  </NuxtLayout>
+</template>
+
+<style>
+html, body , #__nuxt{
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+}
+
+html.dark {
+  background: #222;
+  color: white;
+}
+</style>
